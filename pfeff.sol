2ca798180b9a1e0700d9a4737b56c1a1df936c2b
michael spengler <43786652+michael-spengler@users.noreply.github.com>
2023-10-27 14:52:28 +0200

    Update and rename aldipfefferspray.sol to pfeff.sol

--- pfeff.sol
+++ pfeff.sol
@@ -1,13 +1,16 @@
+// This crypto currency is dedicated to law and order, fairness and justice, peace and love
+// It is inspired by https://twitter.com/ALDI_NordDE 
+
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.20;
 
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Permit.sol";
 
-contract AldiPfefferspray is ERC20, ERC20Permit {
+contract Pfefferspray is ERC20, ERC20Permit {
     constructor()
-        ERC20("AldiPfefferspray", "PFEFF")
-        ERC20Permit("AldiPfefferspray")
+        ERC20("Pfefferspray", "PFEFF")
+        ERC20Permit("Pfefferspray")
     {
         _mint(msg.sender, 80000000 * 10 ** decimals());
     }
